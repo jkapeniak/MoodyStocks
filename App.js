d8c4951d39b2a266386f05c95b1bf7f7a2fa024c
Julius <jkapeniak@gmail.com>
2020-12-19 23:46:08 -0500

    "update state with data from api"

--- App.js
+++ App.js
@@ -3,47 +3,30 @@ import {LineChart, Grid} from 'react-native-svg-charts';
 import {View, Text, Button, Alert} from 'react-native';
 import StackedAreaExample from './components/StackedAreaExample.js';
 
-import axios from "axios";
-
 
 
 class appjs extends React.PureComponent {
-
-   stonks = async () => {
-   // console.log('69')
-    
-    try {
-        const sectorRes = await axios.get(
-          `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo`
-        );
-        //console.log(69)
-        console.log(sectorRes.data)
-      } catch (error) {
-        console.log(error);
-      }
-      
-}
+  
   render() {
     return (
-      <View style ={{flex:1, flexDirection: 'column', justifyContent: 'space-between', }}
-        >
-       
-       <View style={{top:1, padding: 100}}>
-        <Button
-            title="Press"
-            onPress={ this.stonks } //onPress={() => this.state.data[5] = this.state.data[5]+100 }
-          />
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}>
+        <View style={{top: 1, padding: 100}}>
+          
         </View>
-        <View style ={{
-          justifyContent: 'center',
-    //alignItems: 'center',
-    //position: 'absolute', //Here is the trick
-    bottom: -20, //Here is the trick
-  }}>
+        <View
+          style={{
+            justifyContent: 'center',
+            //alignItems: 'center',
+            //position: 'absolute', //Here is the trick
+            bottom: -20, //Here is the trick
+          }}>
           <StackedAreaExample />
         </View>
-
-        
       </View>
     );
   }
--- components/StackedAreaExample.js
+++ components/StackedAreaExample.js
@@ -1,62 +1,167 @@
-import React from 'react'
-import { StackedAreaChart } from 'react-native-svg-charts'
-import * as shape from 'd3-shape'
-
-
+import React from 'react';
+import {StackedAreaChart} from 'react-native-svg-charts';
+import * as shape from 'd3-shape';
+import {View, Text, Button, Alert} from 'react-native';
+import axios from 'axios';
 
 export default class StackedAreaExample extends React.PureComponent {
-    render() {
-        const data = [
+  
+  constructor(){
+      super();
+      this.state = {
+          stockData: [
             {
-                month: new Date(2015, 0, 1),
-                apples: 3840,
-                bananas: 1920,
-                cherries: 960,
-                dates: 400,
+              month: new Date(2015, 0, 1),
+              apples: 3840,
+              bananas: 1920,
+              cherries: 960,
+              dates: 400,
             },
             {
-                month: new Date(2015, 1, 1),
-                apples: 1600,
-                bananas: 1440,
-                cherries: 960,
-                dates: 400,
+              month: new Date(2015, 1, 1),
+              apples: 1600,
+              bananas: 1440,
+              cherries: 960,
+              dates: 400,
             },
             {
-                month: new Date(2015, 2, 1),
-                apples: 640,
-                bananas: 960,
-                cherries: 3640,
-                dates: 400,
+              month: new Date(2015, 2, 1),
+              apples: 640,
+              bananas: 960,
+              cherries: 3640,
+              dates: 400,
             },
-            {
-                month: new Date(2015, 3, 1),
-                apples: 3320,
-                bananas: 480,
-                cherries: 640,
-                dates: 400,
-            },
-        ]
+            
+          ],
+      }
+  }
 
-        const colors = ['#8800cc', '#aa00ff', '#cc66ff', '#eeccff']
-        const keys = ['apples', 'bananas', 'cherries', 'dates']
-        const svgs = [
-            { onPress: () => console.log('apples') },
-            { onPress: () => console.log('bananas') },
-            { onPress: () => console.log('cherries') },
-            { onPress: () => console.log('dates') },
-        ]
-
-        return (
-            <StackedAreaChart
-                style={{ height: 500, }}
-                data={data}
-                keys={keys}
-                colors={colors}
-                curve={shape.curveNatural}
-                showGrid={false}
-                svgs={svgs}
-            />
-        )
+  addItem = () => {
+    let newStock = {
+      month: new Date(2015, 3, 1),
+      apples: 3320,
+      bananas: 480,
+      cherries: 640,
+      dates: 400,
     }
-}
 
+    this.setState({stockData: [...this.state.stockData, newStock]})
+
+  }
+    
+  
+  
+  
+    stonks = async () => {
+   
+    var ret = null;
+    try {
+      const sectorRes = await axios.get(
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo`,
+      );
+      
+      var tSeries = sectorRes.data['Time Series (Daily)'];
+
+      //console.log(Object.size(tSeries));
+      //var tempDataStore = [];
+      let arr = new Array(100);
+      let i = 0;
+      for (var tempData in tSeries) {
+        // console.log('i: ' + i);
+        // console.log('Your Date: ' + tempData);
+
+        const dateFields = tempData.split('-');
+
+        arr[i] = {
+          month: new Date(dateFields[0], dateFields[2], dateFields[1]),
+          apples: parseInt(tSeries[tempData]['4. close']),
+          bananas: 1920,
+          cherries: 960,
+          dates: 400,
+        };
+
+      
+        i++;
+      }
+      
+      for (let i = 0; i < 4; i++) {
+
+        
+        this.state.stockData[i] = arr[i];
+      }
+
+      
+
+      
+    } catch (error) {
+      console.log(error);
+    }
+
+    
+  };
+
+  render() {
+
+    
+    /*const data = [
+      {
+        month: new Date(2015, 0, 1),
+        apples: 3840,
+        bananas: 1920,
+        cherries: 960,
+        dates: 400,
+      },
+      {
+        month: new Date(2015, 1, 1),
+        apples: 1600,
+        bananas: 1440,
+        cherries: 960,
+        dates: 400,
+      },
+      {
+        month: new Date(2015, 2, 1),
+        apples: 640,
+        bananas: 960,
+        cherries: 3640,
+        dates: 400,
+      },
+      {
+        month: new Date(2015, 3, 1),
+        apples: 3320,
+        bananas: 480,
+        cherries: 640,
+        dates: 400,
+      },
+    ];
+        */
+     
+
+    const colors = ['#8800cc', '#aa00ff', '#cc66ff', '#eeccff'];
+    const keys = ['apples', 'bananas', 'cherries', 'dates'];
+    const svgs = [
+      {onPress: () => console.log('apples')},
+      {onPress: () => console.log('bananas')},
+      {onPress: () => console.log('cherries')},
+      {onPress: () => console.log('dates')},
+    ];
+
+    return (
+      <View>
+        <Button
+          title="Press"
+          onPress={this.addItem} //onPress={() => this.state.data[5] = this.state.data[5]+100 }
+        />
+
+        <StackedAreaChart
+          style={{height: 500}}
+          data={this.state.stockData}
+          keys={keys}
+          colors={colors}
+          curve={shape.curveNatural}
+          showGrid={false}
+          svgs={svgs}
+        />
+      </View>
+    );
+  }
+}
